62545f41b4cbd130814b8c609f171b4d9895757b
Carlos Andres Moreno Borja <calitosmore@outlook.com>
2023-08-31 23:31:06 -0500

    Hallar distancia

---
+++ Hallardistancia/Program.cs
@@ -0,0 +1,35 @@
+﻿//Enunciado: 1.Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos.//
+
+
+public class HallarDistanci
+{
+    static void Main(string[] args)
+    {
+        double x1, y1, x2, y2, distancia;
+
+        Console.Write("Ingrese x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese x2: ");
+        x2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese y1: ");
+        y1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese y2: ");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+        distancia = Math.Sqrt(Math.Pow(x2-x1, 2) +  Math.Pow(y2-y1, 2));
+
+        Console.Write("La distancia es: " + distancia);
+        Console.Write("\n");
+    
+    
+    }
+
+
+
+
+
+}
+    
